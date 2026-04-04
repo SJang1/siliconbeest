6cade896dffcce49bf30e41e78778475a55a57f6
SJang1 <siliconsjang@gmail.com>
2026-04-05 00:33:34 +0900

    tests: update migtations

--- siliconbeest/test/worker/helpers.ts
+++ siliconbeest/test/worker/helpers.ts
@@ -63,11 +63,13 @@ import m0018 from '../../migrations/0018_media_proxy_cache.sql?raw';
 import m0020 from '../../migrations/0020_emoji_payload_jit.sql?raw';
 import m0021 from '../../migrations/0021_accounts_emoji_tags.sql?raw';
 import m0022 from '../../migrations/0022_hash_oauth_tokens.sql?raw';
+import m0023 from '../../migrations/0023_feature_gaps.sql?raw';
+import m0024 from '../../migrations/0024_i18n_registration_reason.sql?raw';
 
 const MIGRATIONS: string[] = [
   m0001, m0002, m0003, m0004, m0005, m0006, m0007, m0008,
   m0009a, m0009b, m0010, m0011, m0012, m0013, m0014, m0015,
-  m0016, m0017, m0018, m0020, m0021, m0022,
+  m0016, m0017, m0018, m0020, m0021, m0022, m0023, m0024,
 ];
 
 /**
